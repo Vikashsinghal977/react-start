6bebb40c651ef2e3f35ab3e943c743f933dbef2a
Vikash <vikashsinghal509@gmail.com>
2024-07-01 18:29:54 +0530

    Adding form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 function App() {
   return (
     <>
       <Navbar title="TextUtiles" aboutUs="About Us" />
+      <TextForm/>
       {/* <Navbar /> */}
     </>
   );
