f89bff41a0a721ca37cfd40e646efe8ab705008f
Vikash <vikashsinghal509@gmail.com>
2024-07-02 12:14:16 +0530

    addiing to lower case

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -19,14 +19,20 @@ export default function TextForm(prop) {
   }
   const [text, setText] = useState("Enter text here");
   return (
-    
-    <div>
+    <>
+    <div className='container'>
         <h1>{prop.heading}</h1>
         <div className="mb-3">
             <textarea className="form-control" id="mybox" rows="1" value={text} onChange={handleOnChange}></textarea>
         </div>
-        <button type="button" className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-        <button type="button" className="btn btn-primary" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button type="button" className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button type="button" className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
     </div>
+    <div className="container my-2">
+      <h1>Your text summary </h1>
+      <p>{text.split(" ").length} words, {text.length}, charecter</p>
+    </div>
+
+    </>
   )
 }
